6eff97b65d5be36225c2fe14080e4c1b70491955
Yogesh Ojha <yogeshojha@Yogeshs-MacBook-Air.local>
2017-05-21 10:43:24 +0530

    Hide Softkey Keyboard

--- app/src/main/java/com/yogeshojha/calculator/MainActivity.kt
+++ app/src/main/java/com/yogeshojha/calculator/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.yogeshojha.calculator
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
 
 
@@ -13,25 +17,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mainLayout = findViewById(R.id.mainlinearlayout) as RelativeLayout
         val result = findViewById(R.id.textview_result) as TextView
         val button_add = findViewById(R.id.button_add) as Button
         val button_minus = findViewById(R.id.button_minus) as Button
         val button_multiply = findViewById(R.id.button_multiple) as Button
         val button_divide = findViewById(R.id.button_divide) as Button
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
         button_add.setOnClickListener(View.OnClickListener {
             view -> result.text = if(getNum1().equals(-1) || getNum2().equals(-1))"Either of the values Cannot be empty" else (getNum1() + getNum2()).toString()
+            //hide keyboard on button click
+            imm.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
         })
 
         button_minus.setOnClickListener(View.OnClickListener {
             view -> result.text = if(getNum1().equals(-1) || getNum2().equals(-1))"Either of the values Cannot be empty" else (getNum1() - getNum2()).toString()
+            //hide keyboard on button click
+            imm.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
         })
 
         button_multiply.setOnClickListener(View.OnClickListener {
             view -> result.text = if(getNum1().equals(-1) || getNum2().equals(-1))"Either of the values Cannot be empty" else (getNum1() * getNum2()).toString()
+            //hide keyboard on button click
+            imm.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
         })
         button_divide.setOnClickListener(View.OnClickListener {
             view -> result.text = if(getNum1().equals(-1) || getNum2().equals(-1))"Either of the values Cannot be empty" else if(getNum2() == 0) "Error\nDivisible By Zero" else ((getNum1() / getNum2()).toString())
+            //hide keyboard on button click
+            imm.hideSoftInputFromWindow(mainLayout.getWindowToken(), 0);
 
         })
 
